8cca5abdbc84ee7e3fbd01a0ed662b43a2c85d71
20215ganesh <muzinoasahi@gmail.com>
2023-08-29 09:09:34 +0530

    first commit

---
+++ src/screens/landingPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Loading from '../components/loading';
+function LandingPage() {
+  return (
+    <div className="landing-container">
+    <div className="animated-text">
+      <h1>Welcome to Go Booking</h1>
+      <p>Your Ultimate Booking Experience</p>
+    </div>
+  </div>
+  );
+}
+
+export default LandingPage;
